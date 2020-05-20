69548ef262459a3010e7b764b32004a20b998dbb
chaitanyamehta1 <mehtachaitanya1179@gmail.com>
2020-05-20 15:48:58 +0100

    reqres-serenity-project

---
+++ src/test/java/com/req/res/testbase/TestBase.java
@@ -0,0 +1,20 @@
+package com.req.res.testbase;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+/**
+ * Created by Chaitanya
+ */
+
+public class TestBase {
+
+    @BeforeClass
+    public static void init(){
+        RestAssured.baseURI = "https://reqres.in/";
+        RestAssured.basePath = "/api";
+
+
+    }
+
+}
